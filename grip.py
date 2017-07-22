677753861559ceff68d9e91bdbfd249728901e7e
globedasher <globe.dasher@gmail.com>
2017-07-22 11:25:23 -0700

    sent messages are now added to the message display

--- grip.py
+++ grip.py
@@ -12,6 +12,8 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivy.event import EventDispatcher
+from datetime import datetime
 
 import comms
 
@@ -20,33 +22,41 @@ print("Welcome to Grip Messenger!")
 # NOTE: I am creating a temporily hard coded user name to send and receive
 # messages.
 user_name = "rocko"
+message_stack = []
+
+# TODO: Enter button send function.
+# TODO: Display text in non-editable form.
+# TODO: If user_name is blank, present dialog to request name.
 
 class HomeScreen(Screen):
     """ The HomeScreen widget is the first view loaded of the UI.
     """
-    text_input = ObjectProperty(None)
+    message_input = ObjectProperty(None)
+    message_display = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super(HomeScreen, self).__init__(**kwargs)
-        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
-        self._keyboard.bind(on_key_down=self._on_keyboard_down)
 
-    def _keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-        self._keyboard = None
 
-    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        print(keycode[1])
-        if keycode[1] == 'enter':
-            self.send(self.text_input)
-        return True
+    def on_message_display(self, instance, value):
+        print("The property changed to ", value)
 
     def send(self, data):
         """Send uses the publish() method to send data to the MQTT broker.
         """
-        print(data.text)
-        comms.client.publish(user_name + "/messages", data.text, qos=0)
-        data.text = ''
+        #print(data.text)
+        # TODO: change payload to be dictionary with more attributes for each
+        # message.
+        payload = str(datetime.now()) + " " + user_name + ": " + data.text
+        message_stack.append(payload)
+        print(message_stack)
+        comms.client.publish("grip/messages", payload, qos=0)
+        print(self.message_display.text)
+        print(self.message_input.text)
+        # TODO: Cat new messages onto message stack.
+        # DONE: Refresh the text display.
+        self.message_display.text += self.message_input.text + "\n"
+        data.text = ""
 
 
 # DONE: This class creates the Main Application!
