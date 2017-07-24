6c60d7cd0d9b64f8847836b3c21994e6778d4c50
globedasher <globe.dasher@gmail.com>
2017-07-23 19:57:25 -0700

    major updates; two way comms now working

--- comms.py
+++ comms.py
@@ -1,43 +1,40 @@
 # comms.py to run the communcations of grip.py
 
+#Module imports
 import paho.mqtt.client as mqtt
 
+#Local imports
+from grip import new_message
 
-class Connection():
-    """ This class definition will create a communications module.
-    """
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " +str(rc))
+    print("Let's roll!")
+    # Subscribing in on_connect() means:
+    # If we lose the connection and reconnect, subscriptions will be renewed.
+    print(flags)
+    print(userdata)
+    #client.publish("grip/pub/connect", "Client connect", qos=0, retain=True)
+    client.subscribe("grip/messages", qos=0)
 
-    def __init__(self):
-        """ This is the __init__ function.
-        """
-        print("Created Connection Module.")
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        client.publish("grip/pub/connect", "Disconnected", qos=0, retain=True)
+        print("Unexpected disconnect")
 
-    def on_connect(client, userdata, flags, rc):
-        print ("Connected with result code " +str(rc))
-        # Subscribing in on_connect() means:
-        # If we lose the connection and reconnect, subscriptions will be renewed.
-        print(flags)
-        print(userdata)
-        #client.publish("grip/pub/connect", "Client connect", qos=0, retain=True)
-        client.subscribe("grip/messages", qos=0)
+def on_message(client, userdata, msg):
+    print("on_message")
+    print(msg.topic + " " + str(msg.payload))
+    new_message(msg)
+    HomeScreen.message_display.text += msg.payload
 
-    def on_disconnect(client, userdata, flags, rc):
-        if rc != 0:
-            client.publish("grip/pub/connect", "Disconnected", qos=0, retain=True)
-            print("Unexpected disconnect")
 
-    def on_message(client, userdata, msg):
-        print("on_message")
-        print(msg.topic + " " + str(msg.payload))
-
-print("comms on here?")
-#client = mqtt.Client()
+client = mqtt.Client()
 
 # The following lines define the on_connect and on_message callbacks
-#client.on_connect = on_connect
-#client.on_disconnect = on_disconnect
-#client.on_message = on_message
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
 
-#client.connect("iot.eclipse.org", 1883, 60)
+client.connect("iot.eclipse.org", 1883, 60)
 
-#client.loop_forever()
+client.loop_start()
--- grip.py
+++ grip.py
@@ -1,5 +1,6 @@
 # This is the grip.py file for the matching grip.kv file
 
+#Module imports
 import os, sys, _thread
 
 
@@ -18,38 +19,18 @@ from datetime import datetime
 
 import paho.mqtt.client as mqtt
 
+#Local imports
 #import comms
 
 print("Welcome to Grip Messenger!")
 
 # NOTE: I am creating a temporily hard coded user name to send and receive
 # messages.
-user_name = "rocko"
+user_name = "Rocko"
 message_stack = []
 
-# TODO: Enter button send function.
 # TODO: Display text in non-editable form.
 # TODO: If user_name is blank, present dialog to request name.
-# TODO: Cat new messages onto message stack.
-
-
-
-def on_connect(client, userdata, flags, rc):
-    print ("Connected with result code " +str(rc))
-    # Subscribing in on_connect() means:
-    # If we lose the connection and reconnect, subscriptions will be renewed.
-    #print(flags)
-    #print(userdata)
-    #client.publish("grip/pub/connect", "Client connect", qos=0, retain=True)
-    client.subscribe("grip/messages", qos=0)
-    print("Connected")
-
-def on_disconnect(client, userdata, flags):
-    client.publish("grip/pub/connect", "Disconnected", qos=0, retain=True)
-
-def on_message(client, userdata, msg):
-    print("on_message")
-    print(msg.topic + " " + str(msg.payload))
 
 
 class HomeScreen(Screen):
@@ -58,53 +39,116 @@ class HomeScreen(Screen):
     message_input = ObjectProperty(None)
     message_display = ObjectProperty(None)
 
-    def __init__(self, **kwargs):
+    def __init__(self, client, **kwargs):
         """ Creates all the initial conditions for the HomeScreen.
         """
         # TODO: What does this *super* call do exactly?
         super(HomeScreen, self).__init__(**kwargs)
-        # The following lines define the on_connect and on_message callbacks
 
+        # The following lines define the on_connect and on_message callbacks
+        self.client = client
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_message = self.on_message
+        #self.client.on_publish = self.on_publish
+        self.client.connect("iot.eclipse.org", 1883, 60)
+        self.client.loop_start()
+
+    def on_connect(self, client, userdata, flags, rc):
+        """This callback is triggered when the MQTT client connects to the
+        server.
+        """
+        print("Connected with result code " +str(rc))
+        #print("Let's roll!")
+        # Subscribing in on_connect() means:
+        # If we lose the connection and reconnect, subscriptions will be renewed.
+        #print("User name: %s" % userdata)
+        client.publish(
+                "grip/pub/connect",
+                "%s has connected" % userdata,
+                qos=0,
+                retain=True
+                )
+
+        client.subscribe("grip/messages", qos=0)
+
+    def on_disconnect(self, client, userdata, rc):
+        if rc != 0:
+            client.publish("grip/pub/connect", "Disconnected", qos=0, retain=True)
+            print("Unexpected disconnect")
+        else:
+            print("Disconnect Complete")
+
+    #def on_publish(self, client, userdata, mid):
+        #print(mid)
+
+    # DONE: Cat new messages onto view
+    def on_message(self, client, userdata, msg):
+        """Callback triggered when a message is recieved.
+        """
+        #print(userdata)
+        #print(msg.topic)
+        #print(msg.payload)
+        # Decode the msg.payload from bytearray to UTF-8 then cat onto the
+        # message_display.
+        self.message_display.text += (
+                "%s said: %s \n" % (
+                    userdata,
+                    str(msg.payload.decode('utf-8'))))
 
     def send(self, data):
         """Send uses the publish() method to send data to the MQTT broker.
         """
+        #data is the object passed from the kivy app TextInput function
+
         #print(data.text)
         # TODO: change payload to be dictionary with more attributes for each
         # message.
-        payload = str(datetime.now()) + " " + user_name + ": " + data.text
+        payload = data.text
         #message_stack.append(payload)
         #print(message_stack)
         #print(self.message_display.text)
         #print(self.message_input.text)
-        self.client.publish("grip/messages", payload, qos=0)
+        return_value = self.client.publish("grip/messages", payload, qos=0)
+        #print(return_value)
+        if return_value[0] != 0:
+            print("Send error")
+        else:
+            print("Sent")
+            #print("Messge id: %d" % return_value[1])
         # DONE: Refresh the text display.
-        self.message_display.text += payload + "\n"
+        #self.message_display.text += payload + "\n"
         data.text = ""
 
+    # DONE: Enter button send function.
     def on_enter(self, data):
         """This function allows an enter to send a message.
         """
         self.send(data)
 
     def disconnect(self):
-        print("used method")
+        """Disconnect from the MQTT server and stop the application.
+        """
         self.client.disconnect()
-
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    client.on_message = on_message
-    client.connect("iot.eclipse.org", 1883, 60)
-    client.loop_start()
+        print("Performed disconnect")
+        app.stop()
 
 
 # DONE: This class creates the Main Application!
 class GripApp(App):
     def build(self):
-        return HomeScreen()
+        # Instantiate the client at tag client.
+        client = mqtt.Client(
+                client_id="12999",
+                clean_session=True,
+                userdata=user_name
+                )
+
+        return HomeScreen(client)
 
 
 # The following lines instantiate and run the application.
 if __name__ == "__main__":
-    GripApp().run()
+    app = GripApp()
+    print(app)
+    app.run()
